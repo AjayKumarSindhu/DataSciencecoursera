14ba02d1f8fba958bd0b334f5df616bbbb8c31a2
AjayKumarSindhu <65404760+AjayKumarSindhu@users.noreply.github.com>
2020-06-22 13:21:07 +0530

    Update ProgmngAssignment2.R

--- ProgmngAssignment2.R
+++ ProgmngAssignment2.R
@@ -1,16 +1,18 @@
+##for caching first we make a matrix which can store the required cache data to corresponding computations
 makeMatrix <- function(x = matrix()){
-  m <- NULL
+  m <- NULL     ## with beginning of function x is declared matrix and m is nullified to clear any previously declared value
   set <- function(y){
-    x <<- y
+    x <<- y 
     m <<- NULL
   }
-  get <- function() x
+  get <- function() x ## As x is not part of get environment, x is part of parent environmrnt.as of lexical scopping x is argument submitted
   setSolve <- function(solve) m <<- solve
   getSolve <- function() m
-  list(set = set,get = get,
+  list(set = set,get = get, ## r auttomatically set the final default command as output
        setSolve = setSolve,
        getSolve = getSolve)
 }
+##cacheMatrix function takes only makeMatrix as argument,for any atomic vector x$getSolve is invalid. R gives error
 cacheMatrix <- function(x, ...) {
   m <- x$getSolve()
   if(!is.null(m)) {
